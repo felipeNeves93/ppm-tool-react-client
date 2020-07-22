4d8ba186ae3ca22faec21b7cb27c121d333c2c15
Felipe Neves <felipe.alneves@outlook.com>
2020-07-21 21:33:09 -0300

    Added bootstrap to project

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import ProjectItem from './project/ProjectItem';
+import ProjectItem from './Project/ProjectItem';
 
 class Dashboard extends Component {
     render() {
         return (
             // React components should have only one parent div
             <div>
-                <h1>Welcome to the Dashboard</h1>
+                <h1 className="alert alert-warning">Welcome to the Dashboard</h1>
                 {/* Importing external ProjectItem component here */}
                 <ProjectItem />
             </div>
