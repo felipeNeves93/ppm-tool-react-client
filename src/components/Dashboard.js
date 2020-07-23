7782fb309023fd8d45162d91ad9b913f2116d860
Felipe Neves <felipe.alneves@outlook.com>
2020-07-22 21:01:24 -0300

    Added HTML/CSS to components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
-import ProjectItem from './Project/ProjectItem';
+import ProjectItem from './Project/ProjectItem'
 
 class Dashboard extends Component {
     render() {
         return (
-            // React components should have only one parent div
-            <div>
-                <h1 className="alert alert-warning">Welcome to the Dashboard</h1>
-                {/* Importing external ProjectItem component here */}
-                <ProjectItem />
+            <div className="projects">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="display-4 text-center">Projects</h1>
+                            <br />
+                            <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                                Create a Project
+                            </a>
+                            <br />
+                            <hr />
+                            <ProjectItem />
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
